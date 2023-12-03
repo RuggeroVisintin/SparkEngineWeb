778b4869179e84578760cfb2efb7cb8a305ac854
Ruggero Visintin <ruggero.visintin@docebo.com>
2023-12-03 19:33:02 +0100

    feat(ecs): add render system and shape component

---
+++ test/core/ecs/systems/RenderSystem.test.ts
@@ -0,0 +1,14 @@
+import { RenderSystem, ShapeComponent } from "../../../../src"
+
+describe('core/ecs/systems/RenderSystem', () => {
+    describe('.registerComponent()', () => {
+        const renderSystem = new RenderSystem();
+        const myTestShape = new ShapeComponent();
+
+        it('Should register the component into the RenderSystem components list', () => {
+            renderSystem.registerComponent(myTestShape);
+
+            expect(renderSystem.components).toContain(myTestShape);
+        })
+    })
+})
