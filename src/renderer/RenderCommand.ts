03832564403a4c91e18bfa4d3809464878ddd4c7
Ruggero Visintin <ruggero.visintin@docebo.com>
2023-12-05 00:57:57 +0100

    refactor(rendering): check primitive type

--- src/renderer/RenderCommand.ts
+++ src/renderer/RenderCommand.ts
@@ -13,9 +13,19 @@ export interface RenderCommand {
     execute(ctx: CanvasRenderingContext2D, device: CanvasDevice): void;
 }
 
+/**
+ * Draws a primitive (Rectangle)
+ */
 export class DrawPrimitiveCommand implements RenderCommand {
     public readonly renderCommandID: RenderCommandID = RenderCommandID.RC_DrawPrimitive;
  
+    /** 
+     * @param primitiveType - the type of the Primitive
+     * @param position - the position of the Primitive (x, y)
+     * @param size - the size of the Primitive (width, height)
+     * @param fill - if the Primitive should be filled. Default is true
+     * @param color - the color of the primitive. Default is #d16cd8 
+     */ 
     constructor(
         public readonly primitiveType: PrimitiveType,
         public readonly position: [number, number],
@@ -25,7 +35,8 @@ export class DrawPrimitiveCommand implements RenderCommand {
     ) { }
     
     public execute(ctx: CanvasRenderingContext2D, device: CanvasDevice): void {
-        device.drawRect(ctx, this.position[0], this.position[1], this.size[0], this.size[1]);
+        this.primitiveType === PrimitiveType.Rectangle
+            && device.drawRect(ctx, this.position[0], this.position[1], this.size[0], this.size[1]);
 
         if (this.fill) {
             device.fill(ctx, this.color);
