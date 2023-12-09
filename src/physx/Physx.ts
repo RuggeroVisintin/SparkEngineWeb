82fd78284f968f2e207394c82c0321043e22d8c1
Ruggero Visintin <RuggeroVisintin@users.noreply.github.com>
2023-12-09 23:20:38 +0100

    feat(physx): implement aabb collision detection

--- src/physx/Physx.ts
+++ src/physx/Physx.ts
@@ -3,10 +3,50 @@ export interface PhysicsObject {
     aabb: [number, number, number, number];
 }
 
-export class Physx {
-    public readonly physicsObjects: PhysicsObject[] = [];
+export interface PhysicalObjectCallbackAggregate {
+    /**
+     * The object to push in the Physical World
+     */
+    object: PhysicsObject;
+    /**
+     * Callback to invoke when a collision is detected
+     * 
+     * @param postSimulation 
+     * @returns 
+     */
+    onCollisionCallback: (postSimulation: PhysicsObject) => void;
+}
 
-    public pushPhysicalObject(object: PhysicsObject): void {
-        this.physicsObjects.push(object);
+export class Physx {
+    public readonly physicalWorld: PhysicalObjectCallbackAggregate[] = [];
+
+    public pushPhysicalObject(object: PhysicalObjectCallbackAggregate): void {
+        this.physicalWorld.push(object);
+    }
+
+    public simulate(): void {
+        this.physicalWorld.forEach((physicalObject, idx) => {
+            this.physicalWorld.forEach((otherPhysicalObject, otherIdx) => {
+                if (idx !== otherIdx && this.checkCollision(physicalObject.object, otherPhysicalObject.object)) {
+                    physicalObject.onCollisionCallback(physicalObject.object);
+                }
+            })
+        });
+    }
+
+    private checkCollision(objectA: PhysicsObject, objectB: PhysicsObject): boolean {
+        const [x1, y1, w1, h1] = objectA.aabb;
+        const [x2, y2, w2, h2] = objectB.aabb;
+
+        if (
+            x1 < x2 + w2 &&
+            x1 + w1 > x2 &&
+            y1 < y2 + h2 &&
+            y1 + h1 > y2
+        ) {
+            return true;
+        }
+
+        return false;
     }
 }
--- test/unit/physx/Physx.test.ts
+++ test/unit/physx/Physx.test.ts
@@ -1,16 +1,73 @@
-import { PhysicsObject, Physx } from "../../../src"
+import { PhysicalObjectCallbackAggregate, PhysicsObject, Physx } from "../../../src"
 
 describe('physx/Physx', () => {
+    let physx = new Physx();
+
+    afterEach(() => {
+        physx = new Physx();
+    })
+
     describe('.pushPhysicsObject()', () => {
         it('Should add the physicx object to the physics objects list', () => {
-            const physx = new Physx();
-            const physicsObject: PhysicsObject = {
-                aabb: [10, 10, 25, 25]
+            const physicsObject: PhysicalObjectCallbackAggregate = {
+                object: {
+                    aabb: [10, 10, 25, 25]
+                },
+                onCollisionCallback: (postSimulation) => {},
             };
 
             physx.pushPhysicalObject(physicsObject);
+            expect(physx.physicalWorld).toContain(physicsObject);
+        })
+    })
+
+    describe('.simulate()', () => {
+        it('Should trigger the Physics object callback if a collision is detected', () => {
+            const physicsObject: PhysicalObjectCallbackAggregate = {
+                object: {
+                    aabb: [10, 10, 25, 25]
+                },
+                onCollisionCallback: jest.fn(() => { }),
+            };
+
+            const physicsObject2: PhysicalObjectCallbackAggregate = {
+                object: {
+                    aabb: [15, 15, 25, 25]
+                },
+                onCollisionCallback: jest.fn(() => { }),
+            };
+
+            physx.pushPhysicalObject(physicsObject);
+            physx.pushPhysicalObject(physicsObject2);
+
+            physx.simulate();
             
-            expect(physx.physicsObjects).toContain(physicsObject);
+            expect(physicsObject.onCollisionCallback).toHaveBeenCalledWith(physicsObject.object);
+            expect(physicsObject2.onCollisionCallback).toHaveBeenCalledWith(physicsObject2.object);
+        })
+
+        it('Should not trigger a collision for object that are not colliding', () => {
+            const physicsObject: PhysicalObjectCallbackAggregate = {
+                object: {
+                    aabb: [10, 10, 25, 25]
+                },
+                onCollisionCallback: jest.fn(() => { }),
+            };
+
+            const physicsObject2: PhysicalObjectCallbackAggregate = {
+                object: {
+                    aabb: [45, 45, 25, 25]
+                },
+                onCollisionCallback: jest.fn(() => { }),
+            };
+
+            physx.pushPhysicalObject(physicsObject);
+            physx.pushPhysicalObject(physicsObject2);
+
+            physx.simulate();
+            
+            expect(physicsObject.onCollisionCallback).not.toHaveBeenCalled();
+            expect(physicsObject2.onCollisionCallback).not.toHaveBeenCalled();
         })
     })
 })
