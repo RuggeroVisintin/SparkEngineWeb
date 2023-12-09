28bb8450f26ef784f6408a983d38feefd0ea361f
Ruggero Visintin <RuggeroVisintin@users.noreply.github.com>
2023-12-09 22:46:39 +0100

    refactor(ecs): add type to BoundingBoxComponent

--- src/ecs/components/BoundingBoxComponent.ts
+++ src/ecs/components/BoundingBoxComponent.ts
@@ -1,6 +1,8 @@
+import { Type } from "../../core";
 import { BaseComponent } from "./BaseComponent";
 import { CollisionCallback, ICollidableComponent } from "./interfaces";
 
+@Type('BoundingBoxComponent')
 export class BoundingBoxComponent extends BaseComponent implements ICollidableComponent {
     public onCollisionCb: CollisionCallback | undefined;
 
