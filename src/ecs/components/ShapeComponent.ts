4de13b0986f33cf923ef61217b56d3bf6947f929
Ruggero Visintin <RuggeroVisintin@users.noreply.github.com>
2023-12-12 23:41:23 +0100

    docs: improve documentation

--- src/ecs/components/ShapeComponent.ts
+++ src/ecs/components/ShapeComponent.ts
@@ -9,11 +9,18 @@ import { TransformComponent } from "./TransformComponent";
  */
 @Type('ShapeComponent')
 export class ShapeComponent extends BaseComponent {
+    /**
+     * The primitive type of the shape
+     */
     public shapeType: PrimitiveType = PrimitiveType.Rectangle;
 
     private defaultTransform = new TransformComponent();
     private defaultMaterial = new MaterialComponent();
 
+    /**
+     * When attached to a parent Entity container, it returns its Tranform Component if present.
+     * Otherwise it returns the default transform of the Shape
+     */
     public get transform(): TransformComponent {
         return this.getContainer()?.getComponent('TransformComponent') ?? this.defaultTransform;
     }
@@ -22,6 +29,10 @@ export class ShapeComponent extends BaseComponent {
         return this.getContainer()?.getComponent('MaterialComponent') ?? this.defaultMaterial;
     }
 
+    /**
+     * Put a draw call in the renderer taking into account the component data
+     * @param renderer 
+     */
     public draw(renderer: Renderer): void {
         const { position, size } = this.transform;
         const { opacity } = this.material;
