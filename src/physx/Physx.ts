ad4cd6d91c7e9fcb0f92c40efc42b85d4ae10002
Ruggero Visintin <RuggeroVisintin@users.noreply.github.com>
2024-01-24 05:01:48 +0100

    refactor(physx): return empty vec2 if collision is found

--- src/physx/Physx.ts
+++ src/physx/Physx.ts
@@ -66,9 +66,9 @@ export class Physx {
         const [x2, y2, w2, h2] = objectB.aabb;
 
         if (objectA.isContainer) {
-            return this.checkCollisionContainer(objectA, objectB);
+            return !!this.checkCollisionContainer(objectA, objectB);
         } else if (objectB.isContainer) {
-            return this.checkCollisionContainer(objectB, objectA);
+            return !!this.checkCollisionContainer(objectB, objectA);
         }
 
         // normal collision detection
@@ -84,26 +84,26 @@ export class Physx {
         return false;
     }
 
-    private checkCollisionContainer(container: PhysicsObject, objectB: PhysicsObject): boolean {
+    private checkCollisionContainer(container: PhysicsObject, objectB: PhysicsObject): Vec2 | null {
         const [x1, y1, w1, h1] = container.aabb;
         const [x2, y2, w2, h2] = objectB.aabb;
 
         if (x2 + w2 > x1 + w1) {
-            return true;
+            return new Vec2();
         }
 
         if (y2 + h2 > y1 + h1) {
-            return true;
+            return new Vec2();
         }
 
         if(x2 < x1) {
-            return true;
+            return new Vec2();
         }
 
         if (y2 < y1) {
-            return true;
+            return new Vec2();
         }
         
-        return false;
+        return null;
     }
 }
