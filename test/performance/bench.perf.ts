f0ecbd22089649a9e6c3d25a3237ebed883887a0
Ruggero Visintin <RuggeroVisintin@users.noreply.github.com>
2024-01-24 03:11:00 +0100

    ci: add perf report as pr comment (#74)

--- test/performance/bench.perf.ts
+++ test/performance/bench.perf.ts
@@ -2,6 +2,19 @@ import hierarchyPerf from "./hierarchy.perf";
 import physxPerf from "./physx.perf";
 import rendererPerf from "./renderer.perf";
 
+var fs = require('fs');
+var util = require('util');
+
+var fs = require('fs');
+var util = require('util');
+var logFile = fs.createWriteStream('.benchmark', { flags: 'w' });
+var logStdout = process.stdout;
+
+console.log = function () {
+  logFile.write(util.format.apply(null, arguments) + '\n');
+  logStdout.write(util.format.apply(null, arguments) + '\n');
+}
+
 physxPerf();
 rendererPerf();
 hierarchyPerf();
