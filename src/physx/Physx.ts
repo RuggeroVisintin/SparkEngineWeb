953412a69daa9c10f02a6ef52b65da545abbb294
Ruggero Visintin <RuggeroVisintin@users.noreply.github.com>
2024-01-26 17:48:38 +0100

    fix(physx): fix velocity not accounted in collisions
    With this fix the objects don't clip anymore.
    A following fix will address the issue with the clunky position on a collision

--- src/physx/Physx.ts
+++ src/physx/Physx.ts
@@ -23,6 +23,7 @@ export interface onCollisionCallbackParams {
     otherObject: PhysicsObject;
     simulationResult: {
         velocity: Vec2
+        position: Vec2
     }
     // collisionPlane: Vec2;
 }
@@ -42,7 +43,7 @@ export interface PhysicalObjectCallbackAggregate {
 }
 
 enum CollisionQuadrant {
-    TopLeft,
+    TopLeft = 1,
     TopRight,
     BottomLeft,
     BottomRight
@@ -72,21 +73,35 @@ export class Physx {
     }
 
     private checkCollision(objectA: PhysicsObject, objectB: PhysicsObject): Vec2 | null {
-        const [x1, y1, w1, h1] = objectA.aabb;
-        const [x2, y2, w2, h2] = objectB.aabb;
-
         if (objectA.isContainer) {
             return this.checkCollisionContainer(objectA, objectB);
         } else if (objectB.isContainer) {
             return this.checkCollisionContainer(objectB, objectA);
         }
 
+        return this.checkCollisionClassic(objectA, objectB);
+    }
+
+    private checkCollisionClassic(objectA: PhysicsObject, objectB: PhysicsObject): Vec2 | null {
+        const [x1, y1, w1, h1] = objectA.aabb;
+        const [x2, y2, w2, h2] = objectB.aabb;
+
+        const x1v = x1 + objectA.velocity.x;
+        const y1v = y1 + objectA.velocity.y;
+        const x2v = x2 + objectB.velocity.x;
+        const y2v = y2 + objectB.velocity.y;
+
+        const xw1v = x1v + w1;
+        const yh1v = y1v + h1;
+        const xw2v = x2v + w2;
+        const yh2v = y2v + h2;
+
         // normal collision detection
         if (
-            x1 < x2 + w2 &&
-            x1 + w1 > x2 &&
-            y1 < y2 + h2 &&
-            y1 + h1 > y2
+            x1v < xw2v &&
+            xw1v > x2v &&
+            y1v < yh2v &&
+            yh1v > y2v
         ) {
             return new Vec2();
         }
@@ -98,83 +113,32 @@ export class Physx {
         const [x1, y1, w1, h1] = container.aabb;
         const [x2, y2, w2, h2] = objectB.aabb;
 
-        const xw1 = x1 + w1;
-        const yh1 = y1 + h1;
-        const xw2 = x2 + w2;
-        const yh2 = y2 + h2;
+        const x1v = x1 + container.velocity.x;
+        const y1v = y1 + container.velocity.y;
+        const x2v = x2 + objectB.velocity.x;
+        const y2v = y2 + objectB.velocity.y;
 
-        let result: Vec2 | null = null;
-        let collisionQuadrant: CollisionQuadrant | null = null;
-
-        // another way might be to find the center of mass of each object and get the normal based on the 
-        // position of the objectB related to the center of mass of the container 
-        const containerCenterOfMass = new Vec2(x1 + w1 / 2, y1 + h1 / 2);
-        const objectBCenterOfMass = new Vec2(x2 + w2 / 2, y2 + h2 / 2);
-
-        if (containerCenterOfMass.x <= objectBCenterOfMass.x && containerCenterOfMass.y <= objectBCenterOfMass.y) {
-            collisionQuadrant = CollisionQuadrant.TopRight;
-        } else if (objectBCenterOfMass.x < containerCenterOfMass.x && containerCenterOfMass.y <= objectBCenterOfMass.y) {
-            collisionQuadrant = CollisionQuadrant.TopLeft;
-        } else if(containerCenterOfMass.x <= objectBCenterOfMass.x && objectBCenterOfMass.y < containerCenterOfMass.y) {
-            collisionQuadrant = CollisionQuadrant.BottomRight;
-        } else {
-            collisionQuadrant = CollisionQuadrant.BottomLeft;
-        }
+        const xw1v = x1v + w1;
+        const yh1v = y1v + h1;
+        const xw2v = x2v + w2;
+        const yh2v = y2v + h2;
         
-        if (collisionQuadrant === CollisionQuadrant.TopRight && (xw2 > xw1 || yh2 > yh1)) {
-            // if xw2 - xw1 < yh2 - yh1 - the smaller negative number wins as it's the one with the greatest collision area
-            // normal is a vector facing left
-            // else is a vector facing down
-            // new velocity is the reflection of the object original velocity related to the normal
-            
-            result = Vec2.from(objectB.velocity);
-
-            if (xw2 - xw1 < yh2 - yh1) {
-                result.reflect(Vec2.LEFT);
-            } else {
-                result.reflect(Vec2.DOWN);
-            }
-        } else if (collisionQuadrant === CollisionQuadrant.BottomRight && (xw2 > xw1 || y2 < y1)) {
-            // if xw2 - xw1 < y2 - y1
-            // normal is a vector facing left
-            // else a vector facing up
-            // new velocity is the reflection of the object original velocity related to the normal
-            
-            result = Vec2.from(objectB.velocity);
-
-            if (xw2 - xw1 < y2 - y1) {
-                result.reflect(Vec2.LEFT);
-            } else {
-                result.reflect(Vec2.UP);
-            }
-        } else if (collisionQuadrant === CollisionQuadrant.BottomLeft && (x2 < x1 || y2 < y1)) {
-            // if x2 - x1 > y2 - y1 - on the left the bigger positive number wins
-            // normal is a vector facing right
-            // else a vector facing up
-            // new velocity is the reflection of the object original velocity related to the normal
-            
-            result = Vec2.from(objectB.velocity);
-
-            if (x2 - x1 > y2 - y1) {
-                result.reflect(Vec2.RIGHT);
-            } else {
-                result.reflect(Vec2.UP);
-            }
-        } else if (collisionQuadrant === CollisionQuadrant.TopLeft && (x2 < x1 || yh2 > yh1)) {
-            // if x2 - x1 > yh2 - yh1 - on the left the bigger positive number wins
-            // normal is a vector facing right
-            // else a vector facing down
-            // new velocity is the reflection of the object original velocity related to the normal
-            
-            result = Vec2.from(objectB.velocity);
-
-            if (x2 - x1 > yh2 - yh1) {
-                result.reflect(Vec2.RIGHT);
-            } else {
-                result.reflect(Vec2.DOWN);
-            }
+        if (xw2v > xw1v) {
+            return new Vec2();
         }
-        
-        return result;
+
+        if (yh2v > yh1v) {
+            return new Vec2();
+        }
+
+        if(x2v < x1v) {
+            return new Vec2();
+        }
+
+        if (y2v < y1v) {
+            return new Vec2();
+        }
+
+        return null;
     }
 }
--- test/unit/physx/Physx.test.ts
+++ test/unit/physx/Physx.test.ts
@@ -75,6 +75,34 @@ describe('physx/Physx', () => {
             expect(physicsObject2.onCollisionCallback).toHaveBeenCalledWith(physicsObject.object);
         });
 
+        // it('Should take the object velocity into collision account', () => {
+        //     const physicsObject: PhysicalObjectCallbackAggregate = {
+        //         object: {
+        //             aabb: [0, 100, 16, 150],
+        //             velocity: new Vec2(5)
+        //         },
+        //         onCollisionCallback: jest.fn(() => { }),
+        //     };
+
+        //     const physicsObject2: PhysicalObjectCallbackAggregate = {
+        //         object: {
+        //             aabb: [0, 100, 20, 150],
+        //             velocity: new Vec2()
+        //         },
+        //         onCollisionCallback: jest.fn(() => { }),
+        //     };
+
+        //     physx.pushPhysicalObject(physicsObject);
+        //     physx.pushPhysicalObject(physicsObject2);
+
+        //     physx.simulate();
+            
+        //     expect(physicsObject.onCollisionCallback).toHaveBeenCalledWith(physicsObject2.object);
+        //     expect(physicsObject2.onCollisionCallback).toHaveBeenCalledWith(physicsObject.object);
+        // })
+
+        it.todo('Should calculate and include in the Physics object callback the new velocity resulting from the collision')
+
         it('Should not trigger a collision for object that are not colliding', () => {
             const physicsObject: PhysicalObjectCallbackAggregate = {
                 object: {
@@ -100,6 +128,7 @@ describe('physx/Physx', () => {
             expect(physicsObject.onCollisionCallback).not.toHaveBeenCalled();
             expect(physicsObject2.onCollisionCallback).not.toHaveBeenCalled();
         });
+        
 
         describe('.isContainer', () => {
             it.each([{
