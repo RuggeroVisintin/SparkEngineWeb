7946b05e41d9e2086419db0424eaea7bb5ea00e9
Ruggero Visintin <RuggeroVisintin@users.noreply.github.com>
2024-01-28 20:09:46 +0100

    feat(ecs): add uuid to every component created

--- src/ecs/components/BaseComponent.ts
+++ src/ecs/components/BaseComponent.ts
@@ -1,12 +1,13 @@
+import { v4 as uuid } from 'uuid';
 import { Type } from "../../core";
 import { IEntity } from "../entities";
 import { IComponent } from "./interfaces";
 
-
-
 /** @category Components */
 @Type('BaseComponent')
 export class BaseComponent implements IComponent {
+    public readonly uuid = uuid();
+
     private _container: IEntity | null = null;
 
     public getContainer(): IEntity | null {
--- src/ecs/components/interfaces/IComponent.ts
+++ src/ecs/components/interfaces/IComponent.ts
@@ -1,6 +1,8 @@
 import { IEntity } from "../../entities";
 
 export interface IComponent {    
+    readonly uuid: string;
+
     getContainer(): IEntity | null;
     setContainer(container: IEntity): void;
 }
