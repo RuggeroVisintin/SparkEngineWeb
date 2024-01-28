6cffade5a76bef06cff8d9eb42ce7fd45043c9e6
Ruggero Visintin <RuggeroVisintin@users.noreply.github.com>
2024-01-28 05:02:45 +0100

    refactor(physx): simplify code

--- src/physx/Physx.ts
+++ src/physx/Physx.ts
@@ -88,15 +88,11 @@ export class Physx {
         const [x1, y1, w1, h1] = objectA.aabb;
         const [x2, y2, w2, h2] = objectB.aabb;
 
-        const x1v = x1;
-        const y1v = y1;
-        const xw1v = x1v + w1;
-        const yh1v = y1v + h1;
+        const xw1 = x1 + w1;
+        const yh1 = y1 + h1;
 
-        const x2v = x2;
-        const y2v = y2;
-        const xw2v = x2v + w2;
-        const yh2v = y2v + h2;
+        const xw2 = x2 + w2;
+        const yh2 = y2 + h2;
 
         const result: PhysicsObject = {
             ...objectA
@@ -104,27 +100,26 @@ export class Physx {
 
         // normal collision detection
         if (
-            x1v < xw2v &&
-            xw1v > x2v &&
-            y1v < yh2v &&
-            yh1v > y2v
+            x1 < xw2 &&
+            xw1 > x2 &&
+            x1 < yh2 &&
+            yh1 > y2
         ) {
-            const x1CollisionCount = xw2v - x1v;
-            const xw1CollisionCount = xw1v - x2v;
-            const y1CollisionCount = yh2v - y1v;
-            const yh1CollisionCount = yh1v - y2v;
-
             const absoluteVelocityX = Math.abs(objectA.velocity.x);
             const absoluteVelocityY = Math.abs(objectA.velocity.y);
 
             if (objectA.velocity.x > 0 && absoluteVelocityX > absoluteVelocityY) {
-                result.aabb = [x1v - xw1CollisionCount, y1, w1 , h1];
+                const collisionCount = xw1 - x2;
+                result.aabb = [x1 - collisionCount, y1, w1 , h1];
             } else if (objectA.velocity.x < 0 && absoluteVelocityX > absoluteVelocityY) {
-                result.aabb = [x1v + x1CollisionCount, y1, w1 , h1];
+                const collisionCount = xw2 - x1;
+                result.aabb = [x1 + collisionCount, y1, w1 , h1];
             } else if (objectA.velocity.y > 0 && absoluteVelocityY > absoluteVelocityX) {
-                result.aabb = [x1, y1v - yh1CollisionCount, w1 , h1];
-            } else  if(objectA.velocity.y < 0 && absoluteVelocityY > absoluteVelocityX) {
-                result.aabb = [x1, y1v + y1CollisionCount, w1 , h1];
+                const collisionCount = yh1 - y2;
+                result.aabb = [x1, y1 - collisionCount, w1 , h1];
+            } else if (objectA.velocity.y < 0 && absoluteVelocityY > absoluteVelocityX) {
+                const collisionCount = yh2 - y1;
+                result.aabb = [x1, y1 + collisionCount, w1 , h1];
             }  
 
             return result;
