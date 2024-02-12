d7d5ea17f395c70682dc6c94e8f800fe3bb64d5f
Ruggero Visintin <RuggeroVisintin@users.noreply.github.com>
2024-02-12 22:58:25 +0100

    feat(ecs): add TriggerEntity init config

--- src/ecs/entities/TriggerEntity.ts
+++ src/ecs/entities/TriggerEntity.ts
@@ -1,6 +1,17 @@
+import { UUID } from "crypto";
 import { BoundingBoxComponent, CollisionCallback, CollisionCallbackParams } from "../components";
 import { BaseEntity } from "./BaseEntity";
-import { StaticObject } from "./StaticObject";
+import { StaticObject, StaticObjectProps } from "./StaticObject";
+
+/**
+ * @category Entities
+ */
+export interface TriggerEntityProps extends StaticObjectProps {
+    /**
+     * The target of the trigger component
+     */
+    target: BaseEntity
+}
 
 /**
  * Triggers a callback when a collision with the given target entity is detected
@@ -20,18 +31,18 @@ export class TriggerEntity extends StaticObject {
     private targetComponent: BoundingBoxComponent;
 
     /**
-     * @param target - the target entity to check collisions against
+     * @param props - the init props
      */
-    constructor(target: BaseEntity) {
-        super();
+    constructor(props: TriggerEntityProps) {
+        super(props);
 
-        const targetComponent = target.getComponent<BoundingBoxComponent>('BoundingBoxComponent');
+        const targetComponent = props.target.getComponent<BoundingBoxComponent>('BoundingBoxComponent');
 
         if (!targetComponent) {
             throw new Error('Target entity must have a BoundingBox component attached');
         }
 
-        this.target = target;
+        this.target = props.target;
         this.targetComponent = targetComponent;
         this.boundingBox.onCollisionCb = this.onCollisionHandler.bind(this);
     }
