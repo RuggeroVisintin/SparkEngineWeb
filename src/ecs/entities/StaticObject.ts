d91b0968404e0967db0df4e25079341d6a167d9c
Ruggero Visintin <RuggeroVisintin@users.noreply.github.com>
2024-02-12 22:46:47 +0100

    feat(ecs): add StaticObject init config

--- src/ecs/entities/StaticObject.ts
+++ src/ecs/entities/StaticObject.ts
@@ -1,14 +1,31 @@
-import { BoundingBoxComponent } from "../components";
-import { GameObject } from "./GameObject";
+import { BoundingBoxComponent, BoundingBoxComponentProps } from "../components";
+import { GameObject, GameObjectProps } from "./GameObject";
+
+/**
+ * @category Entities
+ */
+export interface StaticObjectProps extends GameObjectProps {
+    /**
+     * The config of the bounding box component.
+     * Uses default bounding box config if not set
+     */
+    boundingBox?: BoundingBoxComponentProps;
+}
 
 /**
  * @category Entities
  */
 export class StaticObject extends GameObject {
-    public boundingBox = new BoundingBoxComponent();
+    public boundingBox: BoundingBoxComponent;
 
-    constructor() {
+    /**
+     * 
+     * @param props - Optional components configuration
+     */
+    constructor(props?: StaticObjectProps) {
         super();
+
+        this.boundingBox = new BoundingBoxComponent(props?.boundingBox);
         
         this.addComponent(this.boundingBox);
     }
