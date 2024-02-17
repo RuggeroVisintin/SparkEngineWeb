4b28223ee2e808c8f8ca8dfde2bfc2680256bdb0
Ruggero Visintin <RuggeroVisintin@users.noreply.github.com>
2024-02-17 03:19:26 +0100

    docs: fix examples folder structure

--- scripts/serve-examples.sh
+++ scripts/serve-examples.sh
@@ -3,7 +3,6 @@
 set -e
 
 mkdir -p public 
-cp -r examples/ public/examples 
+cp -r examples/* public/ 
 cp -r dist/ public/dist 
-mv public/examples/index.html public/index.html
 node examples.server.js
