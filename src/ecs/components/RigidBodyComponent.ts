e64b4dab66c2425f338cbda03736f7b245fd75cf
Ruggero Visintin <RuggeroVisintin@users.noreply.github.com>
2024-03-12 13:34:35 +0100

    feat(ecs): add RigidBodyComponent (#191)

---
+++ src/ecs/components/RigidBodyComponent.ts
@@ -0,0 +1,10 @@
+import { Type } from "../../core";
+import { BoundingBoxComponent } from "./BoundingBoxComponent";
+
+/**
+ * @category Components
+ */
+@Type('RigidBodyComponent')
+export class RigidBodyComponent extends BoundingBoxComponent {
+
+}
