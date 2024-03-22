692fbcd1a86c923d4297b3ec013733034c8c58a5
Ruggero Visintin <RuggeroVisintin@users.noreply.github.com>
2024-03-22 19:07:58 +0100

    feat(ecs): add diffuse texture loading to MaterialComponent

--- src/ecs/components/MaterialComponent.ts
+++ src/ecs/components/MaterialComponent.ts
@@ -1,4 +1,5 @@
 import { Rgb, Type } from "../../core";
+import { ImageAsset, ImageLoader } from "../../platform";
 import { BaseComponent } from "./BaseComponent";
 
 
@@ -8,6 +9,8 @@ import { BaseComponent } from "./BaseComponent";
 export interface MaterialComponentProps {
     diffuseColor?: Rgb;
     opacity?: number;
+    
+    diffuseTexturePath?: string;
 }
 
 /**
@@ -15,13 +18,29 @@ export interface MaterialComponentProps {
  */
 @Type('MaterialComponent')
 export class MaterialComponent extends BaseComponent {
+    private _diffuseTexture?: ImageAsset;
+
     public diffuseColor: Rgb = Rgb.fromHex('#d16cd8');
     public opacity: number = 100;
+    public readonly diffuseTexturePath?: string;
+
+    get diffuseTexture(): ImageAsset | undefined {
+        return this._diffuseTexture;
+    }
 
     constructor(props?: MaterialComponentProps) {
         super();
 
         if (props?.diffuseColor) this.diffuseColor = Rgb.fromRgb(props.diffuseColor);
         if (props?.opacity) this.opacity = props.opacity;
+        if (props?.diffuseTexturePath) this.diffuseTexturePath = props.diffuseTexturePath;
+    }
+    
+    public load(loader: ImageLoader): void {
+        this.diffuseTexturePath && loader
+            .load(this.diffuseTexturePath)
+            .then((asset) => {
+                this._diffuseTexture = asset;
+            });
     }
 }
--- test/unit/ecs/components/MaterialComponent.test.ts
+++ test/unit/ecs/components/MaterialComponent.test.ts
@@ -1,11 +1,13 @@
-import { MaterialComponent, MaterialComponentProps, Rgb } from "../../../../src"
+import { ImageAsset, ImageLoader, MaterialComponent, MaterialComponentProps, Rgb } from "../../../../src";
+import '../../__mocks__';
 
 describe('/ecs/components/MaterialComponent', () => {
     describe('constructor()', () => {
         it('Should construct a new Material component from given props', () => {
             const init: MaterialComponentProps = {
                 diffuseColor: new Rgb(255, 255, 255),
-                opacity: 100
+                opacity: 100,
+                diffuseTexturePath: 'path/to/texture.png',
             }
         
             const component = new MaterialComponent(init);
@@ -19,6 +21,22 @@ describe('/ecs/components/MaterialComponent', () => {
 
         it('Should set opacity to the max if not specified', () => {
             expect(new MaterialComponent().opacity).toEqual(100);
+        })
+    })
+
+    describe('load()', () => {
+        it('Should load the diffuseTexture asset', (done) => {
+            const component = new MaterialComponent({
+                diffuseTexturePath: 'path/to/texture.png',
+            });
+
+            component.load(new ImageLoader());
+
+            setTimeout(() => {
+                expect(component.diffuseTexture).toBeInstanceOf(ImageAsset);
+                done();
+            }, 10);
+
         })
     })
 })
