9ed02b15a0d27a3805a1958da09147b237661735
Ruggero Visintin <RuggeroVisintin@users.noreply.github.com>
2024-03-24 13:46:24 +0100

    feat(gfx): add drawImage command

--- src/renderer/commands/index.ts
+++ src/renderer/commands/index.ts
@@ -2,3 +2,4 @@ export * from './DrawPrimitiveCommand';
 export * from './RenderCommand';
 export * from './SetBlendingMethodCommand';
 export * from './SetTransformMatrixCommand';
+export * from './DrawImageCommand';
