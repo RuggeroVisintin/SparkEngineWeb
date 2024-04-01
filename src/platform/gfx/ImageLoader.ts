84c8aea6a284bd1147f6f8b48aeff32493edd7bb
Ruggero Visintin <RuggeroVisintin@users.noreply.github.com>
2024-04-01 14:04:03 +0200

    docs: document ImageLoader class

--- src/platform/gfx/ImageLoader.ts
+++ src/platform/gfx/ImageLoader.ts
@@ -8,6 +8,13 @@ import { ImageAsset } from "./ImageAsset";
 export class ImageLoader {
     private _assetsCache: Record<string, ImageAsset> = {};
 
+    /**
+     * Loads the given image from the file system.
+     * If the image is already loaded in the cache it just returns the ImageAsset avoiding loading it from fileSystem
+     * 
+     * @param src - the source path of the asset file
+     * @returns Promise<ImageAsset> - the loaded ImageAsset
+     */
     public async load(src: string): Promise<ImageAsset> {
         if (this._assetsCache[src]) return this._assetsCache[src];
 
