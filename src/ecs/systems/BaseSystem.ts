a58ec438a9636802bac6ee21d099def0d2c83922
Ruggero Visintin <RuggeroVisintin@users.noreply.github.com>
2024-05-09 00:41:50 +0200

    fix(ecs): return early if component not found

--- src/ecs/systems/BaseSystem.ts
+++ src/ecs/systems/BaseSystem.ts
@@ -4,15 +4,31 @@ import { ISystem } from "./ISystem";
 export abstract class BaseSystem<T extends IComponent> implements ISystem {
     public readonly components: T[] = [];
 
+    /**
+     * Adds the component to the system's component list
+     * @param component - The component to be added
+     */
     public registerComponent(component: T): void {
         this.components.push(component);
     }
 
+    /**
+     * Removes the component from the system's component list
+     * @param component - The component to be removed
+     */
     public unregisterComponent(uuid: string): void {
         const componentIndex = this.components.findIndex(component => component.uuid === uuid);
+
+        if (componentIndex === -1) {
+            return;
+        }
 
         this.components.splice(componentIndex, 1);
     }
 
+    /**
+     * triggers an update cycle
+     * @param deltaTime - the elapesd time since last update
+     */
     public abstract update(deltaTime?: number): void;
 }
