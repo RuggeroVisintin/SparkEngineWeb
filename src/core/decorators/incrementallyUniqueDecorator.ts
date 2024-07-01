d155b7b8b8c12206f3350d0ca18b3f94c8d65dc3
Ruggero Visintin <RuggeroVisintin@users.noreply.github.com>
2024-07-01 18:55:53 +0200

    fix(core): fix prototype pollution vuln in `throwIfNotUnique`

--- src/core/decorators/incrementallyUniqueDecorator.ts
+++ src/core/decorators/incrementallyUniqueDecorator.ts
@@ -1,5 +1,11 @@
 const uniqueCounterMap: Record<string, Record<string, number>> = {'global': {}};
 
+const sanitizeScope = (scope: string): string => {
+    if (scope === '__proto__') return '';
+
+    return scope;
+}
+
 /** Decorator function */
 export function incrementallyUnique(value: string) {
     if (uniqueCounterMap['global'][value] === undefined) {
@@ -17,14 +23,14 @@ export interface UniquenessOpts {
 }
 
 export function throwIfNotUnique(value: string, options?: UniquenessOpts) {
-    const scope = options?.scope ?? 'global';
+    const scope = sanitizeScope(options?.scope ?? 'global');
 
     if (uniqueCounterMap[scope]?.[value] !== undefined) {
         throw new Error(`${value} is already used`);
     }
 
     if (!uniqueCounterMap[scope]) {
-        uniqueCounterMap[options?.scope ?? 'global'] = {};
+        uniqueCounterMap[scope] = {};
     }
 
     uniqueCounterMap[scope][value] = 0;
