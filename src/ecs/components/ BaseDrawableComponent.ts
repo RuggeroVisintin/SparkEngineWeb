9e77c913952f5c6384d46b83248f48e645732e99
Ruggero Visintin <RuggeroVisintin@users.noreply.github.com>
2024-07-26 16:07:42 +0200

    feat(ecs): add init props to BaseDrawableComponent

--- src/ecs/components/ BaseDrawableComponent.ts
+++ src/ecs/components/ BaseDrawableComponent.ts
@@ -1,10 +1,22 @@
 import { Renderer } from "../../renderer";
 import { BaseComponent } from "./BaseComponent";
-import { TransformComponent } from "./TransformComponent";
+import { TransformComponent, TransformComponentProps } from "./TransformComponent";
 import { IDrawableComponent } from "./interfaces/IDrawableComponent";
+
+export interface DrawableComponentProps {
+    transform?: TransformComponentProps;
+}
 
 export abstract class BaseDrawableComponent extends BaseComponent implements IDrawableComponent {
     private defaultTransform = new TransformComponent();
+
+    public constructor(props?: DrawableComponentProps) {
+        super();
+
+        if (props?.transform) {
+            this.defaultTransform = new TransformComponent(props.transform);
+        }
+    }
 
     /**
      * When attached to a parent Entity container, it returns its Tranform Component if present.
