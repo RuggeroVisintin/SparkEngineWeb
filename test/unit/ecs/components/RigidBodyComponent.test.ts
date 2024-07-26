bf7e6b4787ab66fdd3ad4161cefa450e4d620bae
Ruggero Visintin <RuggeroVisintin@users.noreply.github.com>
2024-07-26 17:59:11 +0200

    feat(ecs): add .toJson serialization to RigidBodyComponent

--- test/unit/ecs/components/RigidBodyComponent.test.ts
+++ test/unit/ecs/components/RigidBodyComponent.test.ts
@@ -12,4 +12,19 @@ describe('ecs/components/RigidBodyComponent', () => {
             expect(physx.physicalWorld[0].object.rebound).toEqual(1);
         })
     })
+
+    describe('.toJson()', () => {
+        it('Should return a JSON representation of the component', () => {
+            const rigidBody = new RigidBodyComponent();
+            rigidBody.rebound = 0.5;
+
+            expect(rigidBody.toJson()).toEqual({
+                __type: 'RigidBodyComponent',
+                aabb: { x: 0, y: 0, width: 0, height: 0 },
+                isContainer: false,
+                matchContainerTransform: false,
+                rebound: 0.5
+            });
+        })
+    })
 })
