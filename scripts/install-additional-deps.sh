2dde141c10008bdc8c02093d16d50cc8b006a931
Ruggero Visintin <ruggero.visintin@mollie.com>
2025-06-28 12:42:56 +0200

    ci: add adr command

---
+++ scripts/install-additional-deps.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+if [ ! -d ./vendors ]; then
+    echo "Creating vendors directory..."
+    mkdir -p ./vendors
+fi
+
+echo "Installing additional dependencies..."
+echo "Cloning ADR Tools..."
+git clone --depth 1 --branch 3.0.0 https://github.com/npryce/adr-tools.git ./vendors/adr-tools
+
+echo "done"
