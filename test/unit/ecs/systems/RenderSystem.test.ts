2af1d009fc1e704dcb838c8bcfd96afcad86d3be
Ruggero Visintin <9091842+RuggeroVisintin@users.noreply.github.com>
2025-08-06 09:03:49 +0200

    test(ecs): add RenderSystem missing tests

--- test/unit/ecs/systems/RenderSystem.test.ts
+++ test/unit/ecs/systems/RenderSystem.test.ts
@@ -1,19 +1,16 @@
-import { CanvasDevice, RenderSystem, Renderer, ShapeComponent, DOMImageLoader } from "../../../../src";
+import { CanvasDevice, RenderSystem, Renderer, ShapeComponent, DOMImageLoader, CameraComponent, SetTransformMatrixCommand } from "../../../../src";
 
 describe('systems/RenderSystem', () => {
     let renderSystem: RenderSystem;
+    let renderer: Renderer;
+
+    beforeEach(() => {
+        renderer = new Renderer(new CanvasDevice(), { width: 1920, height: 1080 }, new CanvasRenderingContext2D());
+        renderSystem = new RenderSystem(renderer, new DOMImageLoader());
+    })
+
 
     describe('.update()', () => {
-
-        beforeEach(() => {
-            renderSystem = new RenderSystem(new Renderer(new CanvasDevice(), {width: 1920, height: 1080}, new CanvasRenderingContext2D()), new DOMImageLoader())
-        })
-
-        afterEach(() => {
-            jest.clearAllMocks();
-            jest.resetAllMocks();
-        })
-
         it('Should draw renderable object', () => {
             const drawSpy = jest.spyOn(ShapeComponent.prototype, 'draw');
 
@@ -29,7 +26,7 @@ describe('systems/RenderSystem', () => {
         it('Should render objects based on their depthIndex in reverse order (0 rendered last)', () => {
             const component = new ShapeComponent();
             const component2 = new ShapeComponent();
-            
+
             component2.transform.depthIndex = 1;
 
             renderSystem.registerComponent(component);
@@ -39,8 +36,90 @@ describe('systems/RenderSystem', () => {
             const drawSpy2 = jest.spyOn(component2, 'draw');
 
             renderSystem.update();
-            
+
             expect(drawSpy2).toHaveBeenCalledBefore(drawSpy);
         });
-    })
+
+        it('Should render the camera first', () => {
+            const component = new ShapeComponent();
+
+            const drawSpy = jest.spyOn(component, 'draw');
+            const cameraSpy = jest.spyOn(renderSystem.camera, 'draw');
+
+            renderSystem.registerComponent(component);
+            renderSystem.update();
+
+            expect(cameraSpy).toHaveBeenCalled();
+
+            expect(cameraSpy).toHaveBeenCalledBefore(drawSpy);
+        });
+
+        it('Should use the registered camera if available', () => {
+            const camera = new CameraComponent();
+
+            const component = new ShapeComponent();
+            const cameraSpy = jest.spyOn(camera, 'draw');
+
+            renderSystem.registerComponent(camera);
+            renderSystem.registerComponent(component);
+
+            renderSystem.update();
+            expect(cameraSpy).toHaveBeenCalled();
+        });
+
+        it('Should use the default camera if no camera is registered', () => {
+            const component = new ShapeComponent();
+            const cameraSpy = jest.spyOn(renderSystem.camera, 'draw');
+
+            renderSystem.registerComponent(component);
+            renderSystem.update();
+
+            expect(cameraSpy).toHaveBeenCalled();
+        });
+
+        // it('Should use the default camera if the registered camera is not set to draw', () => { 
+        //     const camera = new CameraComponent();
+        //     camera.shouldDraw = false;
+
+        //     const component = new ShapeComponent();
+        //     const cameraSpy = jest.spyOn(camera, 'draw');
+
+        //     renderSystem.registerComponent(camera);
+        //     renderSystem.registerComponent(component);
+
+        //     renderSystem.update();
+
+        //     expect(cameraSpy).not.toHaveBeenCalled();
+        // })
+    });
+
+    describe('.registerComponent()', () => {
+        it('Should register a component', () => {
+            const component = new ShapeComponent();
+            renderSystem.registerComponent(component);
+
+            expect(renderSystem.components).toContain(component);
+        });
+
+        it('Should override the default camera if a CameraComponent is registered', () => {
+            const cameraComponent = new CameraComponent();
+
+            renderSystem.registerComponent(cameraComponent);
+
+            expect(renderSystem.camera).toBe(cameraComponent);
+        });
+    });
+
+    describe('.camera', () => {
+        it('Should return the current camera', () => {
+            const camera = new CameraComponent();
+            renderSystem.registerComponent(camera);
+
+            expect(renderSystem.camera).toBe(camera);
+        });
+
+        it('Should return the default camera if no camera is registered', () => {
+            expect(renderSystem.camera).toBeInstanceOf(CameraComponent);
+        });
+    });
 })
