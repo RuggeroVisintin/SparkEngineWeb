3f0115267e2934b9fa650d90b430d52f214c7108
Ruggero Visintin <9091842+RuggeroVisintin@users.noreply.github.com>
2025-08-19 19:59:29 +0200

    docs(core): add missing documentation

--- src/core/SerializableCallback.ts
+++ src/core/SerializableCallback.ts
@@ -1,3 +1,10 @@
+
+/** 
+ * @category Core
+ * 
+ * SerializableCallback represents a serializable function, which can be serialized to a string and later deserialized back to a function.
+ * 
+ */
 export class SerializableCallback<T extends Function = Function> {
 
     private constructor(
@@ -7,16 +14,33 @@ export class SerializableCallback<T extends Function = Function> {
     ) {
     }
 
+    /**
+     * Creates a new SerializableCallback instance from a function source code.
+     * 
+     * @param source - the source code of the function
+     * @param shouldSerialize - whether the function should be serialized or not
+     * @returns a new SerializableCallback instance
+     */
     static fromString<T extends Function = Function>(source: string, shouldSerialize?: boolean): SerializableCallback<T> {
         const func = new Function(`return (${source})`)();
 
         return new SerializableCallback(func, source, shouldSerialize);
     }
 
+    /**
+     * 
+     * @param func - the source function to create a SerializableCallback from
+     * @param shouldSerialize - whether the function should be serialized or not
+     * @returns a new SerializableCallback instance
+     */
     static fromFunction<T extends Function = Function>(func: T, shouldSerialize?: boolean): SerializableCallback<T> {
         return new SerializableCallback(func, func.toString(), shouldSerialize);
     }
 
+    /**
+     * Converts the SerializableCallback to a string representing its source code.
+     * @returns the source code of the function as a string if serializable, otherwise 'function [not serializable]'
+     */
     toString(): string {
         if (!this.shouldSerialize) {
             return 'function [not serializable]';
@@ -25,6 +49,10 @@ export class SerializableCallback<T extends Function = Function> {
         return this.source
     }
 
+    /**
+     * Converts the SerializableCallback to a JSON object.
+     * @returns the SerializableCallback as a JSON object if serializable
+     */
     toJson(): this | undefined {
         if (!this.shouldSerialize) {
             return undefined;
