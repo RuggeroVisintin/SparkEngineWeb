7a93011644c72cdd48deebc3dffe074bf67c1432
Ruggero Visintin <9091842+RuggeroVisintin@users.noreply.github.com>
2026-08-17 14:11:53 +0200

    refactor(sfx): rename SoundLoader to DOMSoundLoader

--- src/platform/sfx/DOMSoundLoader.ts
+++ src/platform/sfx/DOMSoundLoader.ts
@@ -3,9 +3,9 @@ import { SoundAsset } from "./SoundAsset";
 /**
  * @category Platform
  */
-export class SoundLoader {
+export class DOMSoundLoader {
     public async load(src: string): Promise<SoundAsset> {
-        return new Promise((resolve, reject) => {        
+        return new Promise((resolve, reject) => {
             const audio = new Audio();
             audio.onerror = reject;
             audio.oncanplay = () => {
