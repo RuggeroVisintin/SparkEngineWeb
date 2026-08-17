c7b577cee9b001970abd1ad2c918f3b0868a7f6e
Ruggero Visintin <9091842+RuggeroVisintin@users.noreply.github.com>
2026-08-17 20:53:06 +0200

    test(perf): imrpove rendering performance test

--- test/performance/renderer.perf.ts
+++ test/performance/renderer.perf.ts
@@ -6,18 +6,18 @@ const device = new CanvasDevice();
 const ctx = new Canvas().getContext();
 const renderer = new Renderer(device, { width: 1920, height: 1080 }, ctx);
 
+// Pre-create commands once outside the benchmark
+const preallocatedCommands = Array.from({ length: 500 }, () =>
+    new DrawPrimitiveCommand(PrimitiveType.Rectangle, [0, 0], [1, 1], 1, true)
+);
+
 export default bench([
-    function RendererPushRenderCommand(done: Function) {
-        renderer.pushRenderCommand(new DrawPrimitiveCommand(
-            PrimitiveType.Rectangle,
-            [Math.random(), Math.random()],
-            [Math.random(), Math.random()],
-            1,
-            true
-        ));
-        done();
-    },
-    function RendererEndFrame(done: Function) {
+    function FullFrameRenderCycle(done: Function) {
+        // Push the pre-existing references (isolates buffer and GC overhead)
+        for (let i = 0; i < preallocatedCommands.length; i++) {
+            renderer.pushRenderCommand(preallocatedCommands[i]);
+        }
+
         renderer.endFrame();
         done();
     }
